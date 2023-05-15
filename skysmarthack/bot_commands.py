c2984e6ac124aa87af4496f39002f27c6e6cb6b6
BelGray <goldendaniel186@gmail.com>
2023-05-15 23:12:29 +0300

    Добавлены новые команды в список команд

--- skysmarthack/bot_commands.py
+++ skysmarthack/bot_commands.py
@@ -1,9 +1,15 @@
 from aiogram import types
 
+commands_list = ["/start", "/promo", "/buy_answers", "/profile", "/tools"]
+
 async def set_default_commands(dp):
     """Команды telegram бота"""
     await dp.bot.set_my_commands(
         [
-            types.BotCommand("start", "Главное меню бота")
+            types.BotCommand("start", "ℹ️ Информация о боте"),
+            types.BotCommand("promo", "🔑 Активировать промокод"),
+            types.BotCommand("buy_answers", "💳 Купить ответы на тесты SkySmart"),
+            types.BotCommand("profile", "👤 Профиль пользователя"),
+            types.BotCommand("tools", "⚙️ Панель управления ботом")
         ]
     )
